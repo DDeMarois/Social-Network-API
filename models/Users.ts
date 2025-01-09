210d651b2a99eb9afd4291979457ae02780ddf20
DDeMarois <Drew.DeMarois@du.edu>
2025-01-08 21:22:20 -0700

    added user model

---
+++ models/Users.ts
@@ -0,0 +1,49 @@
+import { Schema, model, Document, ObjectId } from 'mongoose';
+
+interface IUser extends Document {
+    username: string;
+    email: string;
+    thoughts: ObjectId[];
+    friends: ObjectId[];
+    friendCount: number;
+}
+
+const userSchema = new Schema<IUser>({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid email address']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+}, {
+    toJSON: {
+        virtuals: true
+    },
+    toObject: {
+        virtuals: true
+    }
+});
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
