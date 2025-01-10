78627a96e7fd4a44a7433624eebb31a23a4d331b
DDeMarois <Drew.DeMarois@du.edu>
2025-01-09 22:09:52 -0700

    implement user model and CRUD operations; establish database connection

---
+++ routes/api/userRoutes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+const router = Router();
+import {
+getAllUsers,
+getUserById,
+createUser,
+updateUser,
+deleteUser
+} from '../../controllers/userController.js';
+
+router.route('/').get(getAllUsers).post(createUser);
+
+router
+.route('/:userId')
+.get(getUserById)
+.put(updateUser)
+.delete(deleteUser);
+
+export { router as userRouter };
